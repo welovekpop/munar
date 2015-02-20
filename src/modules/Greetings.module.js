4cc2872f3b2e14486da3ea86fe98d923b17b5007
PlugLynn <PlugLynn@Hushmail.com>
2015-02-21 00:58:49 +0100

    extract Greetings to its own module

---
+++ src/modules/Greetings.module.js
@@ -0,0 +1,68 @@
+const debug = require('debug')('sekshi:greeting')
+const assign = require('object-assign')
+
+export default class Greetings {
+
+  constructor(sekshi, options = {}) {
+    this.name = 'Greetings'
+    this.author = 'Sooyou'
+    this.version = '0.1.0'
+    this.description = 'Greets users.'
+
+    this.sekshi = sekshi
+    this.permissions = {
+      greetusers: sekshi.USERROLE.BOUNCER
+    }
+    this.options = assign({
+      greetings: [
+        'Hai @',
+        'Welcome, @!',
+        'hoi @',
+        'Heyho @',
+        'Hej @',
+        '안녕, @!',
+        'Hi @',
+        '/me hugs @'
+      ],
+      emoji: [
+        ':exclamation:',
+        ':purple_heart:',
+        ':blue_heart:',
+        ':v:',
+        ':smirk:',
+        ':laughing:'
+      ]
+    }, options)
+
+    this.autogreet = true
+    this.lastGreeted = -1
+
+    this.greet = this.greet.bind(this)
+    sekshi.on(sekshi.USER_JOIN, this.greet)
+  }
+
+  destroy() {
+    this.sekshi.off(this.sekshi.USER_JOIN, this.greet)
+  }
+
+  greetusers(toggle) {
+    this.autogreet = (toggle.toLowerCase() === 'true' ? true : false)
+  }
+
+  greet(user) {
+    if (!this.autogreet ||
+        this.lastGreeted == user.id ||
+        user.username === this.sekshi.getSelf().username ||
+        !this.sekshi.isModuleEnabled(this.name)) {
+      return
+    }
+
+    let { greetings, emoji } = this.options
+    this.lastGreeted = user.id
+
+    let greeting = greetings[Math.floor(greetings.length * Math.random())]
+    let message = greeting.replace(/@/, `@${user.username}`)
+                + (this.sekshi.isFriend(user.id) ? emoji[Math.floor(emoji.length * Math.random())] : '')
+    setTimeout(this.sekshi.sendChat.bind(this.sekshi, message), 2 * 1000)
+  }
+}
--- src/modules/socials.module.js
+++ src/modules/socials.module.js
@@ -1,39 +1,12 @@
-const debug = require('debug')('sekshi:greeting')
+const debug = require('debug')('sekshi:social-media')
 
 function Socials(sekshi) {
     this.sekshi = sekshi;
 
     this.permissions = {
-        "greetusers": sekshi.USERROLE.BOUNCER,
         "fb": sekshi.USERROLE.NONE,
         "web": sekshi.USERROLE.NONE
     };
-
-    this.autogreet = true;
-    this.lastGreeted = -1;
-
-    this.greetings = [
-        'Hai @',
-        'Welcome, @!',
-        'hoi @',
-        'Heyho @',
-        'Hej @',
-        '안녕, @!',
-        'Hi @',
-        '/me hugs @'
-    ];
-
-    this.emojis = [
-        ':exclamation:',
-        ':purple_heart:',
-        ':blue_heart:',
-        ':v:',
-        ':smirk:',
-        ':laughing:'
-    ];
-
-    this.greet = this.greet.bind(this)
-    sekshi.on(sekshi.USER_JOIN, this.greet);
 }
 
 Socials.prototype.name = "Socials";
@@ -42,24 +15,7 @@ Socials.prototype.version = "0.0.2";
 Socials.prototype.description = "Socials features to greet new users etc";
 
 Socials.prototype.destroy = function() {
-    this.sekshi.removeListener(this.sekshi.USER_JOIN, this.greet);
-};
 
-Socials.prototype.greetusers = function(toggle) {
-    this.autogreet = (toggle.toLowerCase() === "true" ? true : false);
-};
-
-Socials.prototype.greet = function(user) {
-    if(!this.autogreet || this.lastGreeted == user.id || user.username === this.sekshi.getSelf().username || !this.sekshi.isModuleEnabled(this.name))
-        return;
-
-    this.lastGreeted = user.id;
-
-    let greeting = this.greetings[Math.floor(this.greetings.length * Math.random())]
-    let message = greeting.replace(/@/, `@${user.username}`)
-                + (this.sekshi.isFriend(user.id) ?
-                     this.emojis[Math.floor(this.emojis.length * Math.random())] : '')
-    setTimeout(this.sekshi.sendChat.bind(this.sekshi, message), 2 * 1000)
 };
 
 Socials.prototype.fb = function(user) {
