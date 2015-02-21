7b9ebad2fe4376d72d6e5e128cbdf3fb2daca8a1
PlugLynn <PlugLynn@Hushmail.com>
2015-02-21 11:41:53 +0100

    add autoskip for songs in history

---
+++ src/modules/HistorySkip.module.js
@@ -0,0 +1,42 @@
+const debug = require('debug')('sekshi:history-skip')
+const assign = require('object-assign')
+
+export default class HistorySkip {
+
+  constructor(sekshi, options = {}) {
+    this.name = 'HistorySkip'
+    this.author = 'ReAnna'
+    this.version = '0.1.0'
+    this.description = 'Autoskip songs that have been played recently.'
+
+    this.sekshi = sekshi
+    this.options = assign({
+      limit: 50
+    }, options)
+
+    this.permissions = {}
+
+    this._skipping = false
+
+    this.onAdvance = this.onAdvance.bind(this)
+    sekshi.on(sekshi.ADVANCE, this.onAdvance)
+  }
+
+  destroy() {
+    this.sekshi.off(this.sekshi.ADVANCE, this.onAdvance)
+  }
+
+  onAdvance(booth, { media }) {
+    this.sekshi.getRoomHistory((e, history) => {
+      if (e) return debug('error loading history', e)
+      if (history.some(item => item.media.format === media.format && item.media.cid === media.cid)) {
+        let dj = this.sekshi.getCurrentDJ()
+        this.sekshi.onMessage({
+          id: 'sekshi',
+          message: `!lockskip history`
+        })
+      }
+    })
+  }
+
+}
