cd0e4397c3fe758501c0c2a83c17bc211690ec40
PlugLynn <PlugLynn@Hushmail.com>
2015-02-21 15:54:34 +0100

    persist Disconnections to MongoDB

--- src/modules/Disconnect.module.js
+++ src/modules/Disconnect.module.js
@@ -1,69 +1,110 @@
 const SekshiModule = require('../Module')
+const debug = require('debug')('sekshi:disconnect')
+const moment = require('moment')
+const mongoose = require('mongoose')
+
+const Disconnections = mongoose.modelNames().indexOf('Disconnections') === -1
+  ? mongoose.model('Disconnections', {
+      _id: Number, // user id
+      position: Number,
+      time: { type: Date, default: Date.now }
+    })
+  : mongoose.model('Disconnections')
 
 export default class Disconnect extends SekshiModule {
-    constructor(sekshi, options) {
-        this.name = "Disconnect";
-        this.author = "Sooyou";
-        this.version = "0.2.0";
-        this.description = "Provides basic moderation tools";
+  constructor(sekshi, options) {
+    this.name = 'Disconnect'
+    this.author = 'Sooyou'
+    this.version = '1.0.0'
+    this.description = 'Provides basic moderation tools'
 
-        super(sekshi, options)
+    super(sekshi, options)
 
-        this.waitlist = [];
-        this.dropped = [];
+    this.waitlist = []
+    this.dropped = []
 
-        this.permissions = {
-            "dc": sekshi.USERROLE.NONE
-        };
-
-        this.onWaitlistUpdate = this.onWaitlistUpdate.bind(this)
-        this.onUserLeave = this.onUserLeave.bind(this)
-        sekshi.on(sekshi.WAITLIST_UPDATE, this.onWaitlistUpdate);
-        sekshi.on(sekshi.USER_LEAVE, this.onUserLeave);
+    this.permissions = {
+      dc: sekshi.USERROLE.NONE
     }
 
-    destroy() {
-        this.sekshi.removeListener(this.sekshi.WAITLIST_UPDATE, this.onWaitlistUpdate);
-        this.sekshi.removeListener(this.sekshi.USER_LEAVE, this.onUserLeave);
+    this.onWaitlistUpdate = this.onWaitlistUpdate.bind(this)
+    this.onUserLeave = this.onUserLeave.bind(this)
+    sekshi.on(sekshi.WAITLIST_UPDATE, this.onWaitlistUpdate)
+    sekshi.on(sekshi.USER_LEAVE, this.onUserLeave)
+  }
+
+  defaultOptions() {
+    return {
+      timeLimit: 10 * 60 // minutes
     }
+  }
 
-    onWaitlistUpdate(oldWaitlist, newWaitlist) {
-        this.waitlist = oldWaitlist;
+  destroy() {
+    this.sekshi.removeListener(this.sekshi.WAITLIST_UPDATE, this.onWaitlistUpdate)
+    this.sekshi.removeListener(this.sekshi.USER_LEAVE, this.onUserLeave)
+  }
+
+  onWaitlistUpdate(oldWaitlist, newWaitlist) {
+    this.waitlist = oldWaitlist
+  }
+
+  onUserLeave(user) {
+    if(!user)
+      return;
+
+    debug('user disconnected', user.username)
+    for(var i = this.waitlist.length - 1; i >= 0; i--) {
+      if (this.waitlist[i] == user.id) {
+        Disconnections.update(
+          { _id: user.id },
+          { position: i, time: Date.now() },
+          { upsert: true }
+        ).exec()
+        break
+      }
     }
+  }
 
-    onUserLeave(user) {
-        if(!user)
-            return;
+  dc(user) {
+    debug('!dc', user.username)
 
-        for(var i = this.waitlist.length - 1; i >= 0; i--) {
-            if(this.waitlist[i] == user.id) {
+    Disconnections.findById(user.id).exec().then(
+      drop => {
+        if (drop) {
+          debug('found drop')
+          const time = moment(drop.time)
+          const limit = moment().subtract(this.options.timeLimit, 'minutes')
+          const maxDuration = moment.duration(this.options.timeLimit, 'minutes').humanize()
+          if (time.isBefore(limit)) {
+            this.sekshi.sendChat(`@${user.username} Your last disconnect was too long (${time.fromNow(true)}) ago. Disconnects are only valid for ${maxDuration}.`)
+            return
+          }
+          this.sekshi.sendChat(`:white_check_mark: @${user.username} disconnected ${time.fromNow()} ` +
+                               `from position ${drop.position + 1}.`)
 
-                this.dropped.push({
-                    id: this.waitlist[i],
-                    position: i
-                });
-                break;
-            }
+          const removeDrop = () => {
+            Disconnections.remove({ _id: drop.id }).exec().then(
+              () => { debug('removed drop', drop.id) },
+              e => { debug('drop remove error', e) }
+            )
+          }
+          if (this.sekshi.getWaitlist().indexOf(drop.id) < 0) {
+            this.sekshi.addToWaitlist(drop.id, () => {
+              this.sekshi.moveDJ(drop.id, drop.position, removeDrop)
+            })
+          }
+          else {
+            this.sekshi.moveDJ(drop.id, drop.position, removeDrop)
+          }
         }
-    }
-
-    dc(user) {
-
-        for(var i = this.dropped.length - 1; i >= 0; i--) {
-            if(user.id == this.dropped[i].id) {
-                var drop = this.dropped.splice(i, 1)[0];
-
-                if(this.sekshi.getWaitlist().indexOf(drop.id) < 0) {
-                    this.sekshi.addToWaitlist(drop.id, function _miscUserAdded() {
-                        this.moveDJ(drop.id, drop.position);
-                    });
-                } else {
-                    this.sekshi.moveDJ(drop.id, drop.position);
-                }
-
-                break;
-            }
+        else {
+          this.sekshi.sendChat(`@${user.username} I couldn't find you in the database!`)
         }
-    }
-
+      },
+      e => {
+        debug('!dc error', e)
+        this.sekshi.sendChat(`@${user.username} I couldn't find you in the database!`)
+      }
+    )
+  }
 }
