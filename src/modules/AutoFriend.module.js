fed296de33013b8646089f1e70977dc8c84387aa
PlugLynn <PlugLynn@Hushmail.com>
2015-02-26 14:21:39 +0100

    auto-accept friend requests

---
+++ src/modules/AutoFriend.module.js
@@ -0,0 +1,29 @@
+const SekshiModule = require('../Module')
+const debug = require('debug')('sekshi:auto-friend')
+
+export default class AutoFriend extends SekshiModule {
+
+  constructor(sekshi, options) {
+    this.author = 'ReAnna'
+    this.version = '1.0.0'
+    this.description = 'Just wants to be friends with everyone.'
+
+    super(sekshi, options)
+
+    this.onRequest = this.onRequest.bind(this)
+    sekshi.on(sekshi.FRIEND_REQUEST, this.onRequest)
+  }
+
+  destroy() {
+    this.sekshi.removeListener(this.sekshi.FRIEND_REQUEST, this.onRequest)
+  }
+
+  onRequest(user) {
+    debug('new request', user.username)
+    this.sekshi.addFriend(user.id, e => {
+      if (e) debug('friend error', e)
+      else debug('added friend')
+    })
+  }
+
+}
