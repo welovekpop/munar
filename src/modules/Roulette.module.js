444efd903a4fa7d0ec0b5efccb6332fb636e30bd
PlugLynn <i-am-anna@outlook.com>
2015-03-07 16:34:28 +0100

    Roulette fixes
    * fixes crashes when nobody joins
    * remembers last played time across module reloads
    * does not start roulette when one is already running

--- src/modules/Roulette.module.js
+++ src/modules/Roulette.module.js
@@ -7,13 +7,14 @@ export default class Roulette extends SekshiModule {
 
   constructor(sekshi, options) {
     this.author = 'ReAnna'
-    this.version = '0.5.1'
+    this.version = '1.0.0'
     this.description = 'Runs random raffles for wait list position #1.'
 
     super(sekshi, options)
 
     this.permissions = {
       play: sekshi.USERROLE.NONE,
+      withdraw: sekshi.USERROLE.NONE,
       players: sekshi.USERROLE.NONE,
       lastroulette: sekshi.USERROLE.BOUNCER,
       roulette: sekshi.USERROLE.BOUNCER
@@ -27,13 +28,14 @@ export default class Roulette extends SekshiModule {
       duration: 120
     , minPosition: 6
     , winnerPosition: 2
+      // hack for persistent-ish storage of last played time
+    , _lastPlayed: false
     }
   }
 
   init() {
     this._running = false
     this._players = []
-    this._lastPlayed = false
   }
 
   destroy() {
@@ -43,6 +45,9 @@ export default class Roulette extends SekshiModule {
   }
 
   roulette(user) {
+    if (this._running) {
+      return
+    }
     this._running = true
     this._players = []
 
@@ -53,7 +58,7 @@ export default class Roulette extends SekshiModule {
                          `The winner will be moved to spot ${this.options.winnerPosition} in the wait list.` +
                          ` Type "!play" (without quotes) to join. You have ${duration.humanize()}!`)
 
-    this._lastPlayed = moment()
+    this.options._lastPlayed = Date.now()
   }
 
   play(user) {
@@ -63,7 +68,7 @@ export default class Roulette extends SekshiModule {
         , idx = waitlist.indexOf(user.id)
       if (idx === -1) {
         debug('player not in wait list', user.username)
-        this.sekshi.sendChat(`@${user.username} You need to be in the wait list if you want to join the raffle!`, 10 * 1000)
+        this.sekshi.sendChat(`@${user.username} You need to be in the wait list if you want to join the roulette!`, 10 * 1000)
       }
       else if (idx < this.options.minPosition) {
         debug('player too high' /* lol */, user.username)
@@ -76,6 +81,13 @@ export default class Roulette extends SekshiModule {
     }
   }
 
+  withdraw(user) {
+    let i = this._players.indexOf(user)
+    if (this._running && i !== -1) {
+      this._players.splice(i, 1)
+    }
+  }
+
   players() {
     debug('players', this._players.length)
     this.sekshi.sendChat('Roulette players: ' +
@@ -83,8 +95,9 @@ export default class Roulette extends SekshiModule {
   }
 
   lastroulette(user) {
-    if (this._lastPlayed) {
-      this.sekshi.sendChat(`@${user.username} The last roulette was started ${this._lastPlayed.calendar()} (${this._lastPlayed.fromNow()}).`)
+    const lastPlayed = this.options._lastPlayed ? moment(this.options._lastPlayed) : null
+    if (lastPlayed) {
+      this.sekshi.sendChat(`@${user.username} The last roulette was started ${lastPlayed.calendar()} (${lastPlayed.fromNow()}).`)
     }
     else {
       this.sekshi.sendChat(`@${user.username} I don't remember playing roulette!`)
@@ -94,12 +107,17 @@ export default class Roulette extends SekshiModule {
   onEnd() {
     this._running = false
     this._timer = null
-    var winner = this._players[Math.floor(Math.random() * this._players.length)]
-    debug('winner', winner.username)
-    this.sekshi.sendChat(`Roulette winner: @${winner.username}. Congratulations! https://i.imgur.com/TXKz7mt.gif`)
-    this.sekshi.moveDJ(winner.id, this.options.winnerPosition - 1, () => {
-      debug('winner moved')
-    })
+    if (this._players.length === 0) {
+      this.sekshi.sendChat(`Nobody participated in the roulette... Do I get to win now?`)
+    }
+    else {
+      let winner = this._players[Math.floor(Math.random() * this._players.length)]
+      debug('winner', winner.username)
+      this.sekshi.sendChat(`Roulette winner: @${winner.username}. Congratulations! https://i.imgur.com/TXKz7mt.gif`)
+      this.sekshi.moveDJ(winner.id, this.options.winnerPosition - 1, () => {
+        debug('winner moved')
+      })
+    }
     this._players = []
   }
 }
