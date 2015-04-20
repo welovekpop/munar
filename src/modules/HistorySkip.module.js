6b7fcab9a2d33615842f7879dfdc571fa07a3422
PlugLynn <i-am-anna@outlook.com>
2015-04-20 11:43:24 +0200

    remove users from the wait list after subsequent history skips

--- src/modules/HistorySkip.module.js
+++ src/modules/HistorySkip.module.js
@@ -10,14 +10,18 @@ export default class HistorySkip extends SekshiModule {
     this.version = '0.1.0'
     this.description = 'Autoskip songs that have been played recently.'
 
-    this._skipping = false
+    // counts subsequent skips per user. if a user is history-skipped three
+    // times or more, they are removed from the waitlist.
+    // (see options.autoremove)
+    this._skipCount = {}
 
     this.onAdvance = this.onAdvance.bind(this)
   }
 
   defaultOptions() {
     return {
-      limit: 50
+      limit: 50,
+      autoremove: 3
     }
   }
 
@@ -29,15 +33,32 @@ export default class HistorySkip extends SekshiModule {
   }
 
   onAdvance(booth, { media }) {
+    let dj = this.sekshi.getCurrentDJ()
+    const modSkip = this.sekshi.getModule('modskip')
+
+    // nobody is playing anymore
+    if (!dj) return
+
     this.sekshi.getRoomHistory((e, history) => {
       if (e) return debug('error loading history', e)
       if (history.slice(0, this.options.limit)
                  .some(item => item.media.format === media.format && item.media.cid === media.cid)) {
-        let dj = this.sekshi.getCurrentDJ()
-        this.sekshi.onMessage({
-          id: 'sekshi',
-          message: `!lockskip history`
-        })
+        this._skipCount[dj.id] = (this._skipCount[dj.id] || 0) + 1
+        if (this._skipCount[dj.id] >= this.options.autoremove) {
+          if (modSkip) {
+            this.sekshi.sendChat(modSkip._skipMessage(this.sekshi.getSelf(), 'history'))
+          }
+          this.sekshi.removeDJ(dj.id)
+        }
+        else {
+          this.sekshi.onMessage({
+            id: 'sekshi',
+            message: `!lockskip history`
+          })
+        }
+      }
+      else {
+        delete this._skipCount[dj.id]
       }
     })
   }
