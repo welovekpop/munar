6c6a50b95bb10e22d229951919a2e79ea20a8cb7
PlugLynn <rene@kooi.me>
2015-06-07 12:18:31 +0200

    log multiple connection errors (some have more useful info than others)

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const Sekshi = require('./Sekshi')
 const config = require('../config.json')
 const debug = require('debug')('sekshi:app')
 const pkg = require('../package.json')
-const once = require('once')
 
 function start() {
   const sekshi = new Sekshi(config)
@@ -10,13 +9,13 @@ function start() {
   sekshi.start(require('../creds.json'))
 
   let timeout
-  const onError = once(e => {
+  const onError = e => {
     console.error(e.stack || e)
     sekshi.stop(() => {
       console.error('stopped.')
       process.exit(1)
     })
-  })
+  }
 
   sekshi.on(sekshi.CONN_ERROR, e => {
     console.error('connection error')
