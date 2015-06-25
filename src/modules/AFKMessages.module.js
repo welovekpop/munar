838751f3f83d6801ef9d390cb9feb0cf2369f843
PlugLynn <rene@kooi.me>
2015-06-25 16:59:19 +0200

    add module for auto-deleting AFK autoresponder messages

---
+++ src/modules/AFKMessages.module.js
@@ -0,0 +1,29 @@
+const SekshiModule = require('../Module')
+
+export default class AFKMessages extends SekshiModule {
+
+  constructor(sekshi, options) {
+    super(sekshi, options)
+
+    this.author = 'ReAnna'
+    this.version = '1.0.0'
+    this.description = 'Auto-deletes AFK autoresponder messages.'
+
+    this.onMessage = this.onMessage.bind(this)
+  }
+
+  init() {
+    this.sekshi.on(this.sekshi.CHAT, this.onMessage)
+  }
+
+  destroy() {
+    this.sekshi.removeListener(this.sekshi.CHAT, this.onMessage)
+  }
+
+  onMessage(message) {
+    if (message.message.indexOf('[AFK] ') === 0) {
+      this.sekshi.deleteMessage(message.cid)
+    }
+  }
+
+}
