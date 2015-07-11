68698e1370359fed42ac133aee57fc8ef8b2d343
PlugLynn <rene@kooi.me>
2015-07-11 12:21:33 +0200

    youtube-available: add autoskip for nonexistent videos (thanks timberOwl!)

--- src/modules/YouTubeAvailable.module.js
+++ src/modules/YouTubeAvailable.module.js
@@ -8,8 +8,7 @@ export default class YouTubeAvailable extends SekshiModule {
     super(sekshi, options)
 
     this.author = 'ReAnna'
-    this.version = '0.0.1'
-    this.description = 'Notifies staff if a video might not be available.'
+    this.description = 'Autoskips videos that are blocked and notifies staff if a video might not be available everywhere.'
 
     this.onAdvance = this.onAdvance.bind(this)
   }
@@ -17,13 +16,20 @@ export default class YouTubeAvailable extends SekshiModule {
   defaultOptions() {
     return {
       key: false,
-      api: 'https://www.googleapis.com/youtube/v3/videos'
+      api: 'https://www.googleapis.com/youtube/v3/videos',
+
+      skipTerminated: true,
+      notifyRegionBlocked: false,
+      allowedThreshold: 50,
+      blockedThreshold: 100
     }
   }
 
   init() {
     if (!this.options.key) {
-      this.sekshi.sendChat(`@staff The YouTube Availability module needs a YouTube API key. Please set it using '!set youtubeavailable key API_KEY_HERE'.`)
+      this.sekshi.sendChat(
+        `@staff The YouTube Availability module needs a YouTube API key. `
+        `Please set it using '!set youtubeavailable key API_KEY_HERE'.`)
     }
 
     this.sekshi.on(this.sekshi.ADVANCE, this.onAdvance)
@@ -34,20 +40,49 @@ export default class YouTubeAvailable extends SekshiModule {
 
   onAdvance() {
     const media = this.sekshi.getCurrentMedia()
+    const modSkip = this.sekshi.getModule('modskip')
+    const sekshi = this.sekshi.getSelf()
     if (media && media.format === 1 && this.options.key) {
-      let url = `${this.options.api}?id=${media.cid}&part=contentDetails&key=${this.options.key}`
+      let url = `${this.options.api}?id=${media.cid}&part=contentDetails,status&key=${this.options.key}`
       request(url, (e, _, body) => {
         if (e) return debug('yt-api-err', e)
         try {
           let json = JSON.parse(body)
-          let restriction = json.items[0].contentDetails.regionRestriction
-          if (!restriction || restriction.length === 0) return
-          if (restriction.blocked) {
-            this.sekshi.sendChat(`@ReAnna This video is unavailable in ${restriction.blocked.length} countries.`)
+          if (this.options.skipTerminated) {
+            if (json.items.length === 0) {
+              modSkip.lockskip(sekshi, 'unavailable')
+              return
+            }
+
+            let status = json.items[0].status
+            if (status.uploadStatus === 'rejected' || status.embeddable === false) {
+              modSkip.lockskip(sekshi, 'unavailable')
+              return
+            }
           }
-          if (restriction.allowed) {
-            this.sekshi.sendChat(`@ReAnna This video is only available in ${restriction.allowed.length} countries.`)
+          debug('not terminated')
+
+          if (this.options.notifyRegionBlocked) {
+            let restriction = json.items[0].contentDetails.regionRestriction
+            if (!restriction) return
+
+            let { blocked, allowed } = restriction
+            debug('blocked in', blocked ? blocked.length : null)
+            debug('allowed in', allowed ? allowed.length : null)
+            if (blocked && blocked.length > this.options.blockedThreshold) {
+              this.sekshi.sendChat(
+                `@staff This video is unavailable in ${restriction.blocked.length} ` +
+                `countries. It may have to be skipped.`)
+              return
+            }
+            else if (allowed && allowed.length <= this.options.allowedThreshold) {
+              this.sekshi.sendChat(
+                `@staff This video is only available in ${restriction.allowed.length} ` +
+                `countries. It may have to be skipped.`)
+              return
+            }
           }
+          debug('not region blocked')
         }
         catch (e) {}
       })
