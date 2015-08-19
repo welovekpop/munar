61a6fa0c4f3d72a9bd55fa2b5cbdaf037ba59057
ReAnna <rene@kooi.me>
2015-08-19 11:48:00 +0200

    triggers: add $bot, $anyone and $target variables

--- src/modules/Triggers.module.js
+++ src/modules/Triggers.module.js
@@ -2,6 +2,7 @@ const debug = require('debug')('sekshi:triggers')
 const SekshiModule = require('../Module')
 const command = require('../command')
 const { Trigger } = require('../models')
+const random = require('random-item')
 
 export default class Triggers extends SekshiModule {
 
@@ -24,20 +25,44 @@ export default class Triggers extends SekshiModule {
     })
   }
 
-  parse(str, user, target = null) {
+  // Parses a response string with $variables.
+  // Available variables:
+  //  * $user -> Username of the user who used the trigger
+  //  * $dj -> Username of the current DJ
+  //  * $bot -> Username of the bot
+  //  * $artist -> Artist of the current media
+  //  * $title -> Title of the current media
+  //  * $song -> Artist - Title of the current media
+  //  * $anyone -> Username of a random user
+  //  * $target -> Username of the target of this trigger (eg. !slap @TargetUsername)
+  //  * $target{expression} -> Username of the target, or `expression` if no target is given.
+  //                           (The expression can also contain variables.)
+  parse(str, user, targetName = null) {
     let dj = this.sekshi.getCurrentDJ()
     let media = this.sekshi.getCurrentMedia()
+    let bot = this.sekshi.getSelf()
+    let target = targetName ? this.sekshi.getUserByName(targetName) : null
     return str
       .replace(/\$user\b/gi, user.username)
       .replace(/\$dj\b/gi, () => dj ? dj.username : '')
+      .replace(/\$bot\b/gi, bot.username)
       .replace(/\$artist\b/gi, () => media ? media.author : '')
       .replace(/\$title\b/gi, () => media ? media.title : '')
       .replace(/\$song\b/gi, () => media ? `${media.author} – ${media.title}` : '')
+      .replace(/\$anyone\b/gi, () => {
+        let anyone = random(this.sekshi.getUsers())
+        return anyone ? anyone.username : ''
+      })
+      .replace(/\$target(?:\{(.*?)\})?/gi, ($0, defaultTarget) => {
+        return target ? target.username
+             : targetName ? targetName
+             : defaultTarget ? defaultTarget
+             : user.username
+      })
   }
 
   @command('trigger', { role: command.ROLE.BOUNCER })
   createTrigger(user, name, ...response) {
-    console.log(arguments)
     name = name.toLowerCase()
     response = response.join(' ')
     if (name[0] === this.sekshi.delimiter) {
