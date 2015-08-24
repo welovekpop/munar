9df438152d8d18781623e68c70c00f6c1e240685
ReAnna <rene@kooi.me>
2015-08-24 12:36:48 +0200

    triggers: upgrade trigger parsing with nested params and semi-extensible vars

--- src/modules/Triggers.module.js
+++ src/modules/Triggers.module.js
@@ -3,6 +3,48 @@ const SekshiModule = require('../Module')
 const command = require('../command')
 const Trigger = require('../models/Trigger')
 const random = require('random-item')
+const last = require('lodash.last')
+
+// Available variables:
+//  * $user -> Username of the user who used the trigger
+//  * $dj -> Username of the current DJ
+//  * $bot -> Username of the bot
+//  * $artist -> Artist of the current media
+//  * $title -> Title of the current media
+//  * $song -> Artist - Title of the current media
+//  * $anyone -> Username of a random user
+//  * $target -> Username of the target of this trigger (eg. !slap @TargetUsername)
+//  * $target{expression} -> Username of the target, or `expression` if no target is given.
+//                           (The expression can also contain variables.)
+//  * $e{expression} -> URL-encoded version of `expression`.
+const vars = {
+  user({ user }) { return user.username },
+  dj() { return this.getCurrentDJ().username },
+  bot() { return this.getSelf().username },
+  artist() { return this.getCurrentMedia().author },
+  title() { return this.getCurrentMedia().title },
+  song() {
+    let media = this.getCurrentMedia()
+    return `${media.author} – ${media.title}`
+  },
+  anyone() {
+    let anyone = random(this.getUsers())
+    return anyone ? anyone.username : ''
+  },
+  target({ user, args, params }) {
+    let targetName = args.join(' ')
+    let defaultTarget = params.length > 0 ? params[0] : null
+    let target = this.getUserByName(targetName)
+    return target ? target.username
+         : targetName ? targetName
+         : defaultTarget ? defaultTarget
+         : user.username
+  },
+
+  e({ params }) {
+    return encodeURIComponent(params[0])
+  }
+}
 
 export default class Triggers extends SekshiModule {
 
@@ -21,44 +63,71 @@ export default class Triggers extends SekshiModule {
 
   add(name, response) {
     this.addCommand(name, (...params) => {
-      this.sekshi.sendChat(this.parse(response, ...params))
+      this.sekshi.sendChat(this.run(response, ...params))
     })
   }
 
   // Parses a response string with $variables.
-  // Available variables:
-  //  * $user -> Username of the user who used the trigger
-  //  * $dj -> Username of the current DJ
-  //  * $bot -> Username of the bot
-  //  * $artist -> Artist of the current media
-  //  * $title -> Title of the current media
-  //  * $song -> Artist - Title of the current media
-  //  * $anyone -> Username of a random user
-  //  * $target -> Username of the target of this trigger (eg. !slap @TargetUsername)
-  //  * $target{expression} -> Username of the target, or `expression` if no target is given.
-  //                           (The expression can also contain variables.)
-  parse(str, user, targetName = null) {
-    let dj = this.sekshi.getCurrentDJ()
-    let media = this.sekshi.getCurrentMedia()
-    let bot = this.sekshi.getSelf()
-    let target = targetName ? this.sekshi.getUserByName(targetName) : null
-    return str
-      .replace(/\$user\b/gi, user.username)
-      .replace(/\$dj\b/gi, () => dj ? dj.username : '')
-      .replace(/\$bot\b/gi, bot.username)
-      .replace(/\$artist\b/gi, () => media ? media.author : '')
-      .replace(/\$title\b/gi, () => media ? media.title : '')
-      .replace(/\$song\b/gi, () => media ? `${media.author} – ${media.title}` : '')
-      .replace(/\$anyone\b/gi, () => {
-        let anyone = random(this.sekshi.getUsers())
-        return anyone ? anyone.username : ''
-      })
-      .replace(/\$target(?:\{(.*?)\})?/gi, ($0, defaultTarget) => {
-        return target ? target.username
-             : targetName ? targetName
-             : defaultTarget ? defaultTarget
-             : user.username
-      })
+  run(str, user, ...args) {
+    const stringifyList = tokens => tokens.map(stringify).join('')
+    const stringifyBack = token => {
+      return `$${token.name}` + token.params.map(stringifyList).map(str => `{${str}}`).join('')
+    }
+    const stringify = token => {
+      if (token.type === 'raw') return token.value
+      let fn = vars[token.name]
+      if (fn) {
+        return fn.call(this.sekshi, { user, args, params: token.params.map(stringifyList) })
+      }
+      return stringifyBack(token)
+    }
+    let tokens = this.parse(str)
+    return stringifyList(tokens)
+  }
+
+  parse(str) {
+    let tokens = []
+    let lastToken = {}
+    let stack = [ tokens ]
+    let depth = 0
+    let i = 0
+    let chunk
+    while (chunk = str.slice(i)) {
+      if (chunk[0] === '{' && last(tokens).type === 'var') {
+        let param = []
+        let params = last(tokens).params
+        params.push(param)
+        stack.push(tokens = param)
+        depth++
+        i++
+      }
+      else if (chunk[0] === '}' && depth > 0) {
+        stack.pop()
+        tokens = last(stack)
+        depth--
+        i++
+      }
+      else if (chunk[0] === '$') {
+        let match = /^\$(\w+)\b/.exec(chunk)
+        if (match) {
+          tokens.push(lastToken = { type: 'var', name: match[1], params: [] })
+          i += match[0].length
+        }
+        else {
+          tokens.push(lastToken = { type: 'raw', value: chunk[0] })
+          i++
+        }
+      }
+      else {
+        let end$ = chunk.indexOf('$', 1)
+        let endO = chunk.indexOf('{', 1)
+        let endC = depth > 0 ? chunk.indexOf('}', 1) : -1
+        let end = Math.min(...[ end$, endO, endC ].filter(i => i > 0))
+        tokens.push(lastToken = { type: 'raw', value: chunk.slice(0, end) })
+        i += end
+      }
+    }
+    return tokens
   }
 
   @command('trigger', { role: command.ROLE.BOUNCER })
