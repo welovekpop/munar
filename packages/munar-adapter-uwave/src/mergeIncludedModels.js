37e84c7e21b45b23be8924410b48c43a3232b144
ReAnna <renee@kooi.me>
2016-10-03 12:59:13 +0200

    uwave: fix style

--- packages/munar-adapter-uwave/src/mergeIncludedModels.js
+++ packages/munar-adapter-uwave/src/mergeIncludedModels.js
@@ -1,6 +1,6 @@
 import dotprop from 'dot-prop'
 
-export default function mergeIncludedModels({ data, meta, included }) {
+export default function mergeIncludedModels ({ data, meta, included }) {
   Object.keys(meta.included || {}).forEach((type) => {
     meta.included[type].forEach((path) => {
       data.forEach((item) => {
