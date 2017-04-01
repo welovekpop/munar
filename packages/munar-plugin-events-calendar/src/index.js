447e5ab2a16fa10ca95dc4f695f464f970a84654
ReAnna <renee@kooi.me>
2017-04-02 01:03:45 +0200

    events-calendar: init

---
+++ packages/munar-plugin-events-calendar/src/index.js
@@ -0,0 +1,10 @@
+import { Plugin, command, permissions } from 'munar-core'
+
+const debug = require('debug')('munar:events')
+
+export default class EventsCalendar extends Plugin {
+  static description = 'Reaction GIF repository'
+
+  static defaultOptions = {
+  }
+}
