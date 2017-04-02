25a166bb32942709b22ae8932b9674cb4d5dd8ad
ReAnna <renee@kooi.me>
2017-04-02 13:20:28 +0200

    events-calendar: Implement !nextevent.

--- packages/munar-plugin-events-calendar/src/index.js
+++ packages/munar-plugin-events-calendar/src/index.js
@@ -1,10 +1,48 @@
-import { Plugin, command, permissions } from 'munar-core'
+import { Plugin, command } from 'munar-core'
+import googleapis from 'googleapis'
+import pify from 'pify'
+import moment from 'moment'
 
 const debug = require('debug')('munar:events')
+const gcal = googleapis.calendar('v3')
 
 export default class EventsCalendar extends Plugin {
-  static description = 'Reaction GIF repository'
+  static description = 'Google Calendar-based event scheduling.'
 
   static defaultOptions = {
+    key: '',
+    calendar: ''
+  }
+
+  listEvents = pify(gcal.events.list)
+
+  @command('nextevent')
+  async showNextEvent (message) {
+    const body = await this.listEvents({
+      key: this.options.key,
+      calendarId: this.options.calendar,
+      timeMin: new Date().toISOString(),
+      maxResults: 1,
+      singleEvents: true,
+      orderBy: 'startTime'
+    })
+
+    if (!body.items || body.items.length === 0) {
+      message.reply('No scheduled events found.')
+      return
+    }
+
+    const event = body.items[0]
+    const start = moment(event.start.dateTime)
+    const hours = start.diff(moment(), 'hours')
+    const calendar = start.calendar(null, {
+      sameElse: `for MMMM Do`
+    })
+
+    let text = `The next event is ${event.summary}, scheduled ${calendar}.`
+    if (hours < 24) {
+      text += ` (${hours} hours from now)`
+    }
+    message.reply(text)
   }
 }
