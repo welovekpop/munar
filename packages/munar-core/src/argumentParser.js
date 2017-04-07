74e7325f58038226c98828607b86129a0a8b3e42
ReAnna <renee@kooi.me>
2017-04-07 22:16:32 +0200

    Add missing file :|

---
+++ packages/munar-core/src/argumentParser.js
@@ -0,0 +1,33 @@
+import joi from 'joi'
+import pify from 'pify'
+import joinList from 'join-component'
+
+const parser = joi.extend([
+  {
+    base: joi.string().replace(/^@/, ''),
+    name: 'user',
+    rules: []
+  }
+])
+
+const validate = pify(joi.validate)
+
+parser.parse = (input, schema) => {
+  return validate(
+    input,
+    parser.array()
+      .required()
+      .ordered(schema)
+      .error((errors) => {
+        const missing = errors.find((err) => err.type === 'array.includesRequiredKnowns')
+
+        if (missing) {
+          return new Error(`Missing parameters: ${joinList(missing.context.knownMisses)}`)
+        }
+
+        return errors
+      })
+  )
+}
+
+export default parser
