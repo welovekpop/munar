009dc6a94c87503902e84dd6cf24fde9c6020ff7
Renée Kooi <renee@kooi.me>
2018-03-23 15:57:03 +0100

    Babel 7

--- packages/babel-preset-munar/index.js
+++ packages/babel-preset-munar/index.js
@@ -1,12 +1,18 @@
-module.exports = {
-  presets: [
-    require('babel-preset-es2015-node4'),
-    require('babel-preset-es2016'),
-    require('babel-preset-es2017'),
-    require('babel-preset-stage-2')
-  ],
-  plugins: [
-    require('babel-plugin-transform-decorators-legacy').default,
-    require('babel-plugin-transform-export-extensions')
-  ]
+module.exports = (api) => {
+  api.cache.never()
+
+  return {
+    presets: [
+      [require('@babel/preset-env').default, {
+        targets: { node: 4 }
+      }]
+    ],
+    plugins: [
+      require('@babel/plugin-proposal-decorators').default,
+      require('@babel/plugin-proposal-class-properties').default,
+      require('@babel/plugin-proposal-object-rest-spread').default,
+      require('@babel/plugin-proposal-export-default-from').default,
+      require('@babel/plugin-proposal-export-namespace-from').default
+    ]
+  }
 }
