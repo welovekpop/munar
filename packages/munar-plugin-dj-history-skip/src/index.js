3e660e08a0cd851ff9b7059af52031d04fd58e37
Renée Kooi <renee@kooi.me>
2019-07-11 10:08:49 +0200

    dj-history-skip: actually use new isSameSong implementation

--- packages/munar-plugin-dj-history-skip/src/index.js
+++ packages/munar-plugin-dj-history-skip/src/index.js
@@ -2,6 +2,7 @@ import { Plugin, command, permissions } from 'munar-core'
 import lockskip from 'munar-helper-booth-lockskip'
 import delay from 'delay'
 import moment from 'moment'
+import isSameSong from './isSameSong'
 
 const supportsHistory = (adapter) =>
   typeof adapter.getDJHistory === 'function'
@@ -73,27 +74,8 @@ export default class DJHistorySkip extends Plugin {
     }
   }
 
-  isSameSong (a, b) {
-    // TODO abstract into a Media interface with an `isSameSong`-like method?
-    if (a.sourceType !== b.sourceType || a.sourceID !== b.sourceID) {
-      return false
-    }
-    // üWave medias have a start and end property, so an object with the same
-    // sourceType and sourceID can still contain different songs. We'll assume
-    // (quite arbitrarily) that anything that has a 2/3rds overlap in start/end
-    // times is the same song.
-    if ('start' in a && 'end' in a && 'start' in b && 'end' in b) {
-      const duration = a.end - a.start
-      const overlap = Math.abs(a.end - b.start) / duration
-      if (overlap < 2 / 3) {
-        return false
-      }
-    }
-    return true
-  }
-
   isHistoryMatch (a, b) {
-    return this.isSameSong(a.media, b.media) && a.id !== b.id
+    return isSameSong(a.media, b.media) && a.id !== b.id
   }
 
   async maybeSkip (adapter, current) {
